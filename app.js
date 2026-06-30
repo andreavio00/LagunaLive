091076c07934834d622e5f75fd977f5d2fbaf6a1
andreavio00 <andrea.vio@outlook.it>
2026-06-30 23:18:01 +0200

    Add files via upload

--- app.js
+++ app.js
@@ -1,40 +1,12 @@
-async function loadPalazzoCavalli() {
+async function testPuntaSalute() {
   const url =
-    "https://r.jina.ai/http://www.comune.venezia.it/sites/default/files/publicCPSM2/stazioni/temporeale/Palazzo_Cavalli.html";
+    "https://r.jina.ai/http://www.comune.venezia.it/sites/default/files/publicCPSM2/stazioni/temporeale/Punta_Salute.html";
 
   const response = await fetch(url);
   const text = await response.text();
 
-  const rows = text
-    .split("\n")
-    .filter(line => line.startsWith("| 2026-"));
-
-  const lastRow = rows[rows.length - 1];
-
-  const cols = lastRow
-    .split("|")
-    .map(x => x.trim());
-
-  const pressione = cols[2];
-  const temperatura = cols[3];
-  const umidita = cols[4];
-
   document.getElementById("status").innerHTML =
-    "Palazzo Cavalli OK";
-
-  document.getElementById("temp").innerHTML =
-    temperatura + " °C";
-document.getElementById("humidity").innerHTML =
-  umidita + " %";
-
-document.getElementById("pressure").innerHTML =
-  pressione + " hPa";
-
-  console.log({
-    pressione,
-    temperatura,
-    umidita
-  });
+    text.substring(0, 2000).replaceAll("\n", "<br>");
 }
 
-loadPalazzoCavalli();
+testPuntaSalute();
