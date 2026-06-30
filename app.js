e2a98538965cbdb738a5c03f8739372541f4a797
andreavio00 <andrea.vio@outlook.it>
2026-06-30 21:49:30 +0200

    Add files via upload

--- app.js
+++ app.js
@@ -1,16 +1,3 @@
 
-async function testSource(name,url){
-  try{
-    const r=await fetch(url);
-    return `${name}: OK (${r.status})`;
-  }catch(e){
-    return `${name}: ERRORE/CORS`;
-  }
-}
-(async()=>{
- const results=await Promise.all([
-   testSource('CPSM','https://www.comune.venezia.it/'),
-   testSource('MeteoNetwork','https://www.meteonetwork.eu/')
- ]);
- document.getElementById('status').innerHTML=results.join('<br>');
-})();
+document.getElementById('status').innerHTML =
+  'JavaScript caricato correttamente';
