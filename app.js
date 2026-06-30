420ffa84cb44d95788419da5066d08daec779c3c
andreavio00 <andrea.vio@outlook.it>
2026-06-30 22:38:54 +0200

    Add files via upload

--- app.js
+++ app.js
@@ -1,16 +1,12 @@
 async function test() {
-  const url = "https://r.jina.ai/http://www.comune.venezia.it/sites/default/files/publicCPSM2/stazioni/temporeale/Palazzo_Cavalli.html";
+  const url =
+    "https://r.jina.ai/http://www.comune.venezia.it/sites/default/files/publicCPSM2/stazioni/temporeale/Palazzo_Cavalli.html";
 
-  try {
-    const response = await fetch(url);
-    const text = await response.text();
+  const response = await fetch(url);
+  const text = await response.text();
 
-    document.getElementById("status").innerHTML =
-      "Ricevuti " + text.length + " caratteri";
-  } catch (e) {
-    document.getElementById("status").innerHTML =
-      "Errore: " + e.message;
-  }
+  document.getElementById("status").innerHTML =
+    text.substring(0, 1000).replaceAll("\n", "<br>");
 }
 
 test();
