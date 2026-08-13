197c227f44ad436d37ca4e7435cf6313ce92749d
andreavio00 <andrea.vio@outlook.it>
2026-08-13 12:47:06 +0200

    Add files via upload

--- app.js
+++ app.js
@@ -2,7 +2,7 @@
 // fondo alla pagina. Da allineare manualmente al numero della cache
 // in sw.js (CACHE_NAME) quando si rilascia una nuova versione, cosi'
 // i due numeri restano sempre coerenti tra loro.
-const APP_VERSION = "v13";
+const APP_VERSION = "v2.13";
 
 const CAVANIS_URL =
   "https://www.meteonetwork.eu/it/weather-station/vnt375-stazione-meteorologica-di-osservatorio-cavanis-venezia";
